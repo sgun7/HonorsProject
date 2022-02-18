fdd42d5758038a2804eebb5cc6018417d7bbc3a8
sgun7 <60078854+sgun7@users.noreply.github.com>
2022-02-18 14:58:01 -0500

    Create main card component

---
+++ honors-project/src/components/ImageCard1.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      :src=imageSource alt=""
+    ></v-img>
+
+    <v-card-title class="justify-center">
+      {{title}}
+    </v-card-title>
+
+    <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+      >
+        {{action}}
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          {{paragraph}}
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    props: ['title','action', 'paragraph', 'imageSource'],
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
--- honors-project/src/views/Home.vue
+++ honors-project/src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
-  <hello-world />
+  <v-parallax
+    height="1200"
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+  >
+    <v-container class ="my-5">
+
+      <v-layout row wrap justify-space-around>
+          <v-flex xs12 md4>
+            <image-card-1 title="Join the growing community" action="Sign Up" paragraph="We are a network of people who want to make connections and expand our knowledge of the job search.
+          Click the button above to sign up for our program" :imageSource="require('@/assets/blue-light-network-technology-wallpaper-preview.jpg')"></image-card-1>
+          </v-flex>
+          <v-flex xs12 md4>
+            <image-card-1 title="New Card" action="Sign Up" paragraph="We are a network of people who want to make connections and expand our knowledge of the job search.
+          Click the button above to sign up for our program" :imageSource="require('@/assets/blue-light-network-technology-wallpaper-preview.jpg')"></image-card-1>
+          </v-flex>
+      </v-layout>
+
+      <v-layout row wrap>
+         
+      </v-layout>
+
+    </v-container>
+  </v-parallax>
 </template>
 
 <script>
   import HelloWorld from '../components/HelloWorld'
+  import ImageCard1 from '../components/ImageCard1'
 
   export default {
     name: 'Home',
 
     components: {
       HelloWorld,
+      ImageCard1
     },
   }
 </script>
